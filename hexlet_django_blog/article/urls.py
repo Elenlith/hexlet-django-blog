d4f9322b876e2791203e5aedf85420dc6f04d67e
Elenlith <mishchenkov@list.ru>
2023-04-08 16:38:28 +0300

    add articles listing

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from hexlet_django_blog.article import views
 from hexlet_django_blog.article.views import IndexView
 
 urlpatterns = [
@@ -8,4 +7,5 @@ urlpatterns = [
         IndexView.as_view(),
         name = 'article',
         ),
+    path('', IndexView.as_view()),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views import View
+from hexlet_django_blog.article.models import Article
 
 class IndexView(View):
 
-    def get(self, request, tags, article_id):
-        text = f'Статья номер {article_id}. Тег {tags}'
-        return HttpResponse(text)
-
-
-#name = 'article'
-
-#def index(request):
-#    return HttpResponse('article')
-#    return render(request, 'article/index.html', context={'name': name})
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
