020ee3d9c197eff357d9bdfc97362afcfbaa61e7
Elenlith <mishchenkov@list.ru>
2023-04-09 22:23:09 +0300

    add article finding by id

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView
+from hexlet_django_blog.article.views import IndexView, ArticleView
 
 urlpatterns = [
-    path(
-        '<str:tags>/<int:article_id>/',
-        IndexView.as_view(),
-        name = 'article',
-        ),
     path('', IndexView.as_view()),
+    path('<int:id>/', ArticleView.as_view()),
 ]
