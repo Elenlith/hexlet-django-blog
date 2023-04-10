7c8f6a2ace6f24a617ee9329ecac58f76f59cad6
Elenlith <mishchenkov@list.ru>
2023-04-10 12:16:02 +0300

    add article creation functionality

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
 
 class IndexView(View):
 
@@ -19,3 +20,17 @@ class ArticleView(View):
             'article': article,
         })
 
+
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+        return render(request, 'articles/create.html', {'form': form})
